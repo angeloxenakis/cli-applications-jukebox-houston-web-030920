f8d50ab93b4bb974ad20a9a8eeac9b70a3efde59
Angelo Xenakis <angeloxenakis@gmail.com>
2020-03-01 21:44:36 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -60,3 +60,21 @@ def help
      end
    end
  end
+ 
+def run(songs)
+   loop do
+     puts "Please enter a command:"
+     command = gets.chomp
+     case command
+     when 'help'
+       help
+     when 'list'
+       list(songs)
+     when 'play'
+       play(songs)
+     when 'exit'
+       exit_jukebox
+       break
+     end
+   end
+ end
